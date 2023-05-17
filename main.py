897c7f59c03c9331e1b3dcdca5e18c28fbec8db3
Anton Dolgopolik <antondolgopolikv@gmail.com>
2023-05-17 22:50:16 +0300

    Add fix for base

--- main.py
+++ main.py
@@ -26,65 +26,52 @@ def prepare_sub_item(item: SubRipItem):
     return SubItem(text, to_timestamp(item.start), to_timestamp(item.end))
 
 
-def find_time_convergence(start1, end1, start2, end2):
-    st1, et1 = sub_items1[start1].start_time, sub_items1[end1].end_time
-    st2, et2 = sub_items2[start2].start_time, sub_items2[end2].end_time
+def find_time_divergence(start1, end1, start2, end2):
+    st1, et1 = sub_items1[start1].start_time, sub_items1[end1 - 1].end_time
+    st2, et2 = sub_items2[start2].start_time, sub_items2[end2 - 1].end_time
     if et1 <= st2 or et2 <= st1:
-        return 0
+        return 1
     total_dur = max(et1, et2) - min(st1, st2)
     if st1 <= st2 and et2 <= et1:
-        return (et2 - st2) / total_dur
+        return st2 - st1 + et1 - et2
     if st2 <= st1 and et1 <= et2:
-        return (et1 - st1) / total_dur
-    return (min(et1, et2) - max(st1, st2)) / total_dur
-
-
-def find_time_divergence(start1, end1, start2, end2):
-    return 1 - find_time_convergence(start1, end1, start2, end2)
+        return st1 - st2 + et2 - et1
+    return abs(st2 - st1) + abs(et2 - et1)
 
 
 def construct_sentence(start, end, sub_items: List[SubItem]):
     sent = ''
-    for i in range(start, end + 1):
+    for i in range(start, end):
         sent = sent + ' ' + sub_items[i].text
     return sent
 
 
-def find_semantic_divergence(start1, end1, start2, end2):
-    len1 = end1 - start1 + 1
-    len2 = end2 - start2 + 1
+def find_semantic_divergence_metric(start1, end1, start2, end2):
+    len1 = end1 - start1
+    len2 = end2 - start2
     sent1 = construct_sentence(start1, end1, sub_items1)
     sent2 = construct_sentence(start2, end2, sub_items2)
-
     if not tensor_flag1[start1][len1]:
         tensor_dp1[start1][len1] = model.encode(sent1, convert_to_tensor=True)
         tensor_flag1[start1][len1] = True
     if not tensor_flag2[start2][len2]:
         tensor_dp2[start2][len2] = model.encode(sent2, convert_to_tensor=True)
         tensor_flag2[start2][len2] = True
-
-    divergence = (1 - util.cos_sim(tensor_dp1[start1][len1], tensor_dp2[start2][len2])[0][0]) / 2
-    return divergence
+    return (1 - util.cos_sim(tensor_dp1[start1][len1], tensor_dp2[start2][len2])[0][0]) / 2
 
 
 def find_divergence_metric(start1, end1, start2, end2):
-    if start1 == end1 or start2 == end2:
-        return 0.6
     time_div = find_time_divergence(start1, end1, start2, end2)
-    semantic_div = find_semantic_divergence(start1, end1, start2, end2)
-    assert 0 <= time_div <= 1
-    assert 0 <= semantic_div <= 1
-    if start1 == 13 and end1 == 14 and start2 == 21 and end2 == 22:
-        print('true 0: {} + {} = {}'.format(time_div, semantic_div, time_div * 0.6 + semantic_div * 0.4))
-    if start1 == 13 and end1 == 14 and start2 == 21 and end2 == 23:
-        print('false 0: {} + {} = {}'.format(time_div, semantic_div, time_div * 0.6 + semantic_div * 0.4))
-    return time_div * 0.6 + semantic_div * 0.4
+    semantic_div_metric = find_semantic_divergence_metric(start1, end1, start2, end2)
+    # print(time_div * semantic_div_metric)
+    # print(time_div * semantic_div_metric)
+    return time_div * semantic_div_metric
 
 
 def find_best_len(pos1, pos2, step):
     if step > 4:
         return 0, 0, 0.0
-    best_divergence = 100
+    best_divergence = 100000000
     best_l1, best_l2 = 0, 0
     for i in range(pos1, min(pos1 + 3, len(sub_items1)) + 1):
         for j in range(pos2, min(pos2 + 3, len(sub_items2)) + 1):
@@ -92,7 +79,7 @@ def find_best_len(pos1, pos2, step):
             l2 = j - pos2
             if (l1 == 0 and l2 != 1) or (l1 != 1 and l2 == 0):
                 continue
-            val1 = find_divergence_metric(pos1, i - 1, pos2, j - 1)
+            val1 = find_divergence_metric(pos1, i, pos2, j)
             val2 = find_best_len(i, j, step + 1)[2]
             divergence_metric = val1 + val2
             if divergence_metric < best_divergence:
@@ -102,11 +89,11 @@ def find_best_len(pos1, pos2, step):
 
 
 def sync():
-    pos1, pos2 = 0, 0
+    pos1, pos2 = 16, 26
     while pos1 < len(sub_items1) and pos2 < len(sub_items2):
         len1, len2, divergence = find_best_len(pos1, pos2, 0)
-        sent1 = construct_sentence(pos1, pos1 + len1 - 1, sub_items1)
-        sent2 = construct_sentence(pos2, pos2 + len2 - 1, sub_items2)
+        sent1 = construct_sentence(pos1, pos1 + len1, sub_items1)
+        sent2 = construct_sentence(pos2, pos2 + len2, sub_items2)
         print('-----------------------------------')
         print(sent1 + ' -------> ' + sent2)
         if len1 > 0:
@@ -141,9 +128,9 @@ tensor_dp2 = np.zeros((len(sub_items2), 5), dtype=Tensor)
 tensor_flag1 = np.zeros((len(sub_items1), 5), dtype=bool)
 tensor_flag2 = np.zeros((len(sub_items2), 5), dtype=bool)
 
-print(find_best_len(13, 21, 0))
+# print(find_best_len(13, 21, 0))
 
-# sync()
+sync()
 # 375 + 167 / 4094
 # 00:04:33,917        00:04:38,555
 # 00:04:34,294        00:04:38,388
